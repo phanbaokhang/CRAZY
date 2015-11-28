3d49f2aacb5e45a0608cbdef1250cf3fe53d08d5
Khoa Phan <moneysearch5287@gmail.com>
2015-11-28 18:09:51 +0700

    Initial Commit

---
+++ CRAZY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CRAZY
+//
+//  Created by Admin on 11/28/15.
+//  Copyright © 2015 khoaphan_vn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
